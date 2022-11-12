2d46fc4bebde125b3945c63bdcc00c36071bd029
vikasrushi <0xvikas@gmail.com>
2022-11-12 09:08:30 +0530

    isWhitelisted & isPaused Test Added

--- test/ERC20Testing.js
+++ test/ERC20Testing.js
@@ -23,24 +23,46 @@ contract("ERC20 Testing", (accounts) => {
     );
     assert(contractBalance.toNumber() === balanceArray.toNumber());
   });
-
+  let statusIspaused;
   it("Checking for Contract Paused Status", async () => {
-    const status = await instance.isPaused();
-    assert(status === true);
+    statusIspaused = await instance.isPaused();
+    assert(statusIspaused === true);
   });
 
-  //   it("Transferring Token to another wallet Checking", async () => {
-  //     // intially sending allowance to accounts 2 from accounts 0 and sending 100 tokens
-  //     const account = await instance.transferFromIssuer(accounts[2], 100);
-  //     console.log(account);
-  //   });
-
-  it("Checking Intial Issuers Accounts", async () => {
+  it("Checking WhiteListing Accounts", async () => {
     const issuerAddress = await instance.issuer();
-    assert(issuerAddress === accounts[0]);
+    // here issuer can't use their wallet to send money
+    assert(issuerAddress != accounts[2]);
+    // Checking for new account is WhiteListed default it should be false
+    const intialStatus = await instance.isWhitelisted(accounts[2]);
+    assert(intialStatus == false);
+    const WhiteListingAccount = await instance.addToWhitelist(accounts[2]);
+    const statusforWhiteListing = WhiteListingAccount.receipt.status;
+    const FinalStatus = await instance.isWhitelisted(accounts[2]);
+    assert(FinalStatus === statusforWhiteListing);
   });
-  it("Checking Intial for registrarAddress", async () => {
-    const registrarAddress = await instance.registrar();
-    assert(registrarAddress === accounts[1]);
+
+  it("Checking changing paused true to false ", async () => {
+    const unpauseContract = await instance.unpauseContract();
+    assert(unpauseContract.receipt.status === true);
+    statusIspaused = await instance.isPaused();
+    assert(statusIspaused === false);
   });
+  it("Transferring Token to another wallet Checking", async () => {});
+
+  // it("Checking Intial Issuers Accounts", async () => {
+  //   const issuerAddress = await instance.issuer();
+  //   assert(issuerAddress === accounts[0]);
+  // });
+  // it("Checking Intial for registrarAddress", async () => {
+  //   const registrarAddress = await instance.registrar();
+  //   assert(registrarAddress === accounts[1]);
+  // });
 });
+
+// add to whitelist false => true
+// ispaused == > false intial and change it to unPaused public trnxs
+// isWhitelist address intial should be false
+// transferfrom issuer value
+// balance should be equal to totalamount- trnx amount
+// its true
